4d168ea5df2f0f91972ca1dc5a5d8a8440bbc6cd
Khanmasum123 <141628265+Khanmasum123@users.noreply.github.com>
2025-09-01 16:28:16 +0530

    First commit - todo app

---
+++ script.js
@@ -0,0 +1,25 @@
+let inputBox=document.getElementById("inp")
+let inputBut=document.getElementById("but")
+let ul=document.getElementById("add")
+
+inputBut.addEventListener("click",function(){
+if(inputBox.value===""){
+    alert("Fields box")
+}
+else{
+    let li=document.createElement("li")
+    
+    li.textContent=inputBox.value;
+    ul.appendChild(li);
+    inputBox.value="";
+
+    let delBut=document.createElement("button");
+    delBut.textContent="Remove";
+    li.appendChild(delBut);
+
+    delBut.addEventListener("click",function(){
+        li.remove()
+    })
+
+}
+})
